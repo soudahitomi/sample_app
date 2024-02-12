792e5f317421bd567ff9e7d79e49070fdfb8167a
HitomiSouda <154737068+soudahitomi@users.noreply.github.com>
2024-02-12 01:37:07 +0000

    app1-2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,12 +3,25 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
+  end
+
   def index
+    @lists = List.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
